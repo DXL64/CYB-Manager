bb7c701995ae75eac5b990a5022e1719f3219337
trananhhh <trananhprince@gmail.com>
2024-10-07 16:44:42 +0700

    Set limit to 1000

--- composables/services/student.service.ts
+++ composables/services/student.service.ts
@@ -4,33 +4,37 @@ import { stringify } from "querystring";
 import { Student } from "@/models/student.model";
 import config from "@/config/config";
 
-const baseUrl = `${config.backend.end_point}/v1`
+const baseUrl = `${config.backend.end_point}/v1`;
 
 const StudentService: IBaseService<Student> = {
-    List: async (query?: {}): Promise<ResponseModel<Student>> => {
-        let path = `${baseUrl}/students`
-        if (query) {
-            const queryString = stringify(query);
-            path = `${baseUrl}/students?${queryString}`
-        }
-        const result = await axiosClient.get(path);
-        return result.data;
-    },
-    Get: async (id: string): Promise<Student> => {
-        const result = await axiosClient.get(`${baseUrl}/students/${id}`)
-        return result.data
-    },
-    Update: async (id: string, model: Student): Promise<Student> => {
-        const formData = ObjectToForm(model)
-        console.log(formData)
-        const result = await axiosClient.put(`${baseUrl}/students/${id}`, formData)
-        return result.data
-    },
-    Create: async (model: Student): Promise<Student> => {
-        const formData = ObjectToForm(model)
-        const result = await axiosClient.post(`${baseUrl}/students`, formData)
-        return result.data
+  List: async (query): Promise<ResponseModel<Student>> => {
+    let path = `${baseUrl}/students`;
+    if (query) {
+      const queryString = stringify(query);
+      path = `${baseUrl}/students?${queryString}`;
     }
-}
+    const result = await axiosClient.get(path, {
+      params: {
+        limit: 1000,
+      },
+    });
+    return result.data;
+  },
+  Get: async (id: string): Promise<Student> => {
+    const result = await axiosClient.get(`${baseUrl}/students/${id}`);
+    return result.data;
+  },
+  Update: async (id: string, model: Student): Promise<Student> => {
+    const formData = ObjectToForm(model);
+    console.log(formData);
+    const result = await axiosClient.put(`${baseUrl}/students/${id}`, formData);
+    return result.data;
+  },
+  Create: async (model: Student): Promise<Student> => {
+    const formData = ObjectToForm(model);
+    const result = await axiosClient.post(`${baseUrl}/students`, formData);
+    return result.data;
+  },
+};
 
-export default StudentService
+export default StudentService;
